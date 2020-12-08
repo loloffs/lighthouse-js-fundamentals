a96898ea5e235e11962ea8c057c428b1b2eaf1bc
Luke <lukeoloffs@gmail.com>
2020-12-08 20:33:04 +0000

    Is this number even or odd

---
+++ odd.js
@@ -0,0 +1,9 @@
+function isOdd(num) {
+  if (num % 2 === 0) {
+    console.log(num + ' is an even number.')
+  } else if (num % 2 > 0) {
+    console.log(num + ' is an odd number');
+  }
+}
+
+isOdd(3);
